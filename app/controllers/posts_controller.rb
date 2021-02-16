dfbdd377b1815897c3171150800b4a40b630d41b
naoya0118 <yn.emerald.brand@gmail.com>
2021-02-16 17:17:58 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,16 @@ class PostsController < ApplicationController
     Post.create(content: params[:content])
     redirect_to action: :index
   end
+
+  def checked
+    Post.create(content: params[:content])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post:item }
+  end
 end
